43f4f3c3061ba6bc2a43197b9820e1257a37cf52
frostg-012 <optimus7907@gmail.com>
2022-02-09 13:40:00 +0700

    juice: Cleanup device tree  * Move keylayout into configs folder  * Move bluetooth into configs folder  * Move properties into configs/properties folder  * Move config.fs into configs folder
    Signed-off-by: lynnnnzx <rifqikanekii140202@gmail.com>
    Change-Id: Iacc1777c4e095082c5908d3d6c8c7290a963e773

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -41,7 +41,7 @@ TARGET_OTA_ASSERT_DEVICE := lime,citrus,lemon,pomelo,juice
 AUDIO_FEATURE_ENABLED_HDMI_SPK := true
 
 # Bluetooth
-BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/bluetooth/include
+BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := $(DEVICE_PATH)/configs/bluetooth/include
 TARGET_FWK_SUPPORTS_FULL_VALUEADDS := true
 TARGET_USE_QTI_BT_STACK := true
 
@@ -67,7 +67,7 @@ HWUI_COMPILE_FOR_PERF := true
 TARGET_ENABLE_MEDIADRM_64 := true
 
 # Filesystem
-TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
+TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/configs/config.fs
 
 # FM
 AUDIO_FEATURE_ENABLED_FM_POWER_OPT := true
@@ -177,6 +177,10 @@ TARGET_USE_AOSP_SURFACEFLINGER := true
 
 # Platform
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno610
+
+# Properties
+TARGET_PRODUCT_PROP += $(DEVICE_PATH)/configs/properties/product.prop
+TARGET_SYSTEM_PROP += $(DEVICE_PATH)/configs/properties/system.prop
 
 # Recovery
 TARGET_RECOVERY_FSTAB := $(DEVICE_PATH)/rootdir/etc/fstab.default
