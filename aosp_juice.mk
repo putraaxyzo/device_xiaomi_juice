8699e565f37f4f324b73d85d1adf36cc98f8693b
putraaxyzo <wibowoyunantosrisaputra@gmail.com>
2022-03-10 14:06:44 +0700

    juice : Bringup RAVEN-OS
    Signed-off-by: putraaxyzo <wibowoyunantosrisaputra@gmail.com>

--- aosp_juice.mk
+++ aosp_juice.mk
@@ -6,17 +6,17 @@
 
 $(call inherit-product, device/xiaomi/juice/device.mk)
 
-# Inherit some common Fluid stuff.
-$(call inherit-product, vendor/fluid/config/common_full_phone.mk)
-TARGET_INCLUDE_LIVE_WALLPAPERS := false
-TARGET_GAPPS_ARCH := arm64
+# Inherit some common Raven-OS stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-# Bootanimation
-TARGET_SCREEN_WIDTH := 1080
-TARGET_SCREEN_HEIGHT := 2340
+# Raven Stuff .
+RAVEN_BUILD_TYPE := UNOFFICIAL
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_PIXEL_CHARGER := true
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := fluid_juice
+PRODUCT_NAME := aosp_juice
 PRODUCT_DEVICE := juice
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := sm6115
@@ -24,4 +24,3 @@ PRODUCT_MANUFACTURER := Xiaomi
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-BUILD_FINGERPRINT := "google/coral/coral:12/SP1A.210812.016.A1/7796139:user/release-keys"
