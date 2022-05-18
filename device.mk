538c8e6c6d09236ebcbf36401e742c51aa5b9d8f
putraaxyzo <wibowoyunantosrisaputra@gmail.com>
2022-05-18 07:42:55 +0000

    juice: overlay-awaken: Add maintainer strings
    Signed-off-by: putraaxyzo <wibowoyunantosrisaputra@gmail.com>

--- device.mk
+++ device.mk
@@ -133,7 +133,8 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
+    $(LOCAL_PATH)/overlay \
+    $(LOCAL_PATH)/overlay-awaken
 
 # Power
 PRODUCT_PACKAGES += \
